cc218e483d8a75e6274de2576f8b1cf6aa23d42b
lienmly <lienmly@live.com>
2016-10-11 14:54:22 -0600

    Comments editted

--- RiotAPI/main.py
+++ RiotAPI/main.py
@@ -1,18 +1,18 @@
+#=========================================
+# Lien Ly - main function call 
+#=========================================
 import getMatchAuto as a
 from RiotAPI import RiotAPI
-# from APIGrabber import APIGrabber
 
 def main():
     # api = RiotAPI('314c9e1a-a062-40e0-acc4-c4ff55eefb2b')
+
     # r = api.get_summoner_by_name('lucas1432')
-    # print(r)
     # r = api.requestRankedData('62280492',str('314c9e1a-a062-40e0-acc4-c4ff55eefb2b'))
-    # print(r)
     # r = api.get_match_info('2141841204')
     # print(r)
 
     a.getGoldFrames()
-    
     
 if __name__ == "__main__":
     main()
