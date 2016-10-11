162c880dca3240a5291fd73116925a4494bb8d22
lienmly <lienmly@live.com>
2016-10-11 14:56:02 -0600

    Comments editted

--- RiotAPI/RiotContants.py
+++ RiotAPI/RiotContants.py
@@ -1,11 +1,15 @@
 URL = {
     'base': 'https://{proxy}.api.pvp.net/api/lol/{region}/{url}',
+
     #/api/lol/{region}/v1.4/summoner/by-name/{summonerNames}
     'summoner_by_name': 'v{version}/summoner/by-name/{names}',
+
     #/api/lol/{region}/v2.5/league/by-summoner/{summonerIds}
     'get_ranked_match': 'v{version}/league/by-summoner/{summonerid}',
+
     #/api/lol/{region}/v2.2/match/{matchId}?includeTimeline={timeline_bool}
     'match_data': 'v{version}/match/{matchid}?includeTimeline={timeline_bool}',
+
     #/api/lol/{region}/v2.2/matchlist/by-summoner/{summonerId}
     'match_history': 'v{version}/matchlist/by-summoner/{summonerId}'
     }
